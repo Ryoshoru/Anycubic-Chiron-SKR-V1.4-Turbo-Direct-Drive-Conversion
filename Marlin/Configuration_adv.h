c9937ae053c3a51f8cac0ae23db719e73fdb9c96
René Erler <rene.erler@student.uibk.ac.at>
2022-01-04 17:33:09 +0100

    adjusted motor current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2638,7 +2638,7 @@
 #define INTERPOLATE true
 
 #if AXIS_IS_TMC(X)
-#define X_CURRENT 800            // (mA) RMS current. Multiply by 1.414 for peak current.
+#define X_CURRENT 960            // (mA) RMS current. Multiply by 1.414 for peak current.
 #define X_CURRENT_HOME X_CURRENT // (mA) RMS current for sensorless homing
 #define X_MICROSTEPS 128         // 0..256
 #define X_RSENSE 0.11
@@ -2656,7 +2656,7 @@
 #endif
 
 #if AXIS_IS_TMC(Y)
-#define Y_CURRENT 800
+#define Y_CURRENT 960
 #define Y_CURRENT_HOME Y_CURRENT
 #define Y_MICROSTEPS 32
 #define Y_RSENSE 0.11
@@ -2674,7 +2674,7 @@
 #endif
 
 #if AXIS_IS_TMC(Z)
-#define Z_CURRENT 800
+#define Z_CURRENT 960
 #define Z_CURRENT_HOME Z_CURRENT
 #define Z_MICROSTEPS 128
 #define Z_RSENSE 0.11
@@ -2683,7 +2683,7 @@
 #endif
 
 #if AXIS_IS_TMC(Z2)
-#define Z2_CURRENT 800
+#define Z2_CURRENT 960
 #define Z2_CURRENT_HOME Z2_CURRENT
 #define Z2_MICROSTEPS Z_MICROSTEPS
 #define Z2_RSENSE 0.11
