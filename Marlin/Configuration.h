05550e03320e5cece79dde8b11fa648db77d6007
René Erler <rene.erler@student.uibk.ac.at>
2022-01-04 23:09:36 +0100

    enabled adaptive step smoothing adjusted jerk

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1046,8 +1046,8 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-#define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge
-#define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle \
+#define JUNCTION_DEVIATION_MM 0.01 // (mm) Distance from real junction edge
+#define JD_HANDLE_SMALL_SEGMENTS   // Use curvature estimation instead of just the junction angle \
                                     // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
 
